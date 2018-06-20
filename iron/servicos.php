794ff7154469445bfff6eb3d2a3bcc32d209b882
silvioamp <silvioamp@gmail.com>
2018-06-20 20:06:54 -0300

    site_iron_frontend

---
+++ iron/servicos.php
@@ -0,0 +1,100 @@
+<html>
+<head>
+<link href="style_central_assinante.css" rel="stylesheet" type="text/css">
+<link href="style_acesso_cliente_iron.css" rel="stylesheet" type="text/css">
+<link href="../css/iron/style_index.css" rel="stylesheet" type="text/css">
+<link href="../css/iron/style_servicos.css" rel="stylesheet" type="text/css">
+<meta name="title" content="Iron">
+<link href="estilo.css" rel="stylesheet" type="text/css">
+<meta name="author" content="Iron - http://www.iron.com.br">
+<meta name="description" content="description">
+<meta name="keywords" content="keywords">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta name="language" content="pt-br">
+<meta name="robots" content="index,follow">
+<meta name="revisit-after" content="30 days">
+<link href="favicon.ico" rel="icon" type="../image/x-icon">
+<link href="favicon.ico" rel="shortcut icon" type="../image/x-icon">
+<script language="JavaScript" type="text/javascript" src="../js/function.js"></script>
+<style type="text/css">
+.submenu{display: none;}
+</style>
+
+<title>Iron Internet</title>
+<link href="style_screen.css" rel="stylesheet" type="text/css">
+<link href="../jir.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="../jir.js"></script>
+
+</head>
+<body>
+<div id=container>
+	<div id="black-bar">
+		<ul>
+			<li><a href="teste.html">Página Inicial</a></li>
+			<li><a href="empresa.php">A Empresa</a></li>
+			<li><a href="servicos.php">Serviços</a></li>
+			<li><a href="contato.php">Contato</a></li>
+			
+		</ul>
+	</div>
+
+<div id="webmail">
+	<form action="../loga_webmail.php" target="_blank" method="post" name="implogin">		
+	     	<img src="../img/iron/webmail.gif" alt="WebMail" class="img_webmail">
+		<span>E-mail:</span>
+		<input tabindex="1" name="imapuser" type="text" id="email" value="">
+		<span>Senha:</span>
+		<input type="password" tabindex="2" name="pass" id="senha">
+
+		<span>Modelo:</span>
+		<select tabindex="4" id="select_view" name="select_view">
+			<option value="dimp">Dinâmico</option>
+			<option value="imp">Tradicional</option>
+		</select>
+
+		<input type="submit" value="Entrar" class="btn_entrar">
+		<div id="lembrar">
+			<input class="lembrar" type="checkbox" name="lembrar" value="ok"> <p>Lembrar e-mail</p>
+		</div>
+	</form>
+</div>
+
+<!-- Termino do cabecalho -->
+
+	
+	<div id="conteudo">
+		<div id="esqueda">
+        	<div class="links">
+
+			</div>
+		</div>	
+		<div id="meio">
+        	<img src="../img/iron/banner_servicos.gif" alt="" class="banner_servicos" />
+			<h1><img src="../img/iron/icone_tit_servicos.gif" alt="Servicos" /> <a href="servicos.php" class="migalha">Serviços</a></h1>
+            <div class="servicosatalho">
+            <h2>Selecione um dos serviços abaixo para conhecê-lo detalhadamente:</h2>
+            	<a href="hospedagem.php" title="Hospedagem"><img src="../img/iron/servicos_hospedagem.gif" alt="Hospedagem" /><b class="hospedagem">Hospedagem</b><p>Hospede seu site com segurança <br />e tecnologia de ponta.</p></a>
+                <a href="criacao_sites.php" title="Criação de Sites" class="border0"><img src="../img/iron/servicos_criacao.gif" alt="Criação de Sites" /><b class="criacao">Criação de Sites</b><p>Desenvolvemos sites de acordo com o seu <br />gosto e com a identidade da sua empresa.</p></a>
+
+				<a href="banda_larga_residencial.php" title="Banda Larga Residencial"><img src="../img/iron/servicos_bandaresidencial.gif" alt="Banda Larga Residencial" /><b class="bandaresidencial">Banda Larga Residencial</b><p>Acesso banda larga de alta qualidade <br />com um excelente custo-benef�cio.</p></a>
+                <a href="banda_larga_comercial.php" title="Banda Larga Comercial" class="border0"><img src="../img/iron/servicos_bandacomercial.gif" alt="Banda Larga Comercial" /><b class="bandacomercial">Banda Larga Comercial</b><p>Para empresas que buscam acesso <br />� internet em alta velocidade.</p></a>
+
+                <a href="banda_larga_web1000.php" title="Banda Larga Web1000"><img src="../img/iron/servicos_web1000.gif" alt="Banda Larga Web1000" /><b class="bandaweb1000">Banda Larga Web1000</b><p>Uma alternativa com caracteristicas<br/> voltadas a usu�rios dom�sticos.</p></a>
+                <a href="banda_larga_comercial_ind.php" title="Banda Larga Comercial Individualizado" class="border0"><img src="../img/iron/servicos_bandacomercial_ind.gif" alt="Banda Larga Comercial Individualizado" /><b class="bandacomercialind">Banda Larga Individualizado</b><p>Para empresas de grande porte ou conjuntos<br />comerciais em condom�nios sem viabilidade.</p></a>
+
+                <a href="speedy_home.php" title="Speedy Home"><img src="../img/iron/servicos_speedyhome.gif" alt="Speedy Home" /><b class="speedyhome">Speedy Home</b><p>Acesso banda larga em parceria <br />com a Telef�nica.</p></a>
+                <a href="speedy_negocios.php" title="Speedy Negócios" class="border0"><img src="../img/iron/servicos_speedynegocios.gif" alt="Speedy Negócios" /><b class="speedynegocios">Speedy Negócios</b><p>Acesso banda larga para as empresas <br />em parceria com a Telefônica. &nbsp;&nbsp;&nbsp;</p></a>
+
+    		</div>
+    	</div>
+
+	</div>
+	<div id="rodape">
+		<ul>
+			<li><strong>Rua Tolentino Filgueiras cj. 41 - Gonzaga - Santos/SP - Central de Atendimento: (13) 3229-9000</strong><br>
+			© 2018 Iron Internet - Todos os direitos reservados.</li>
+		</ul>
+	</div>	
+</div>
+</body>
+</html>
